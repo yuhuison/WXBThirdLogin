4ed681ab3c1ec1af780c802724a1fc5d2f91aefe
yuhuison <yuhuison@live.com>
2020-03-12 13:51:23 +0800

    up to 1.5

--- HOOKHOST/HOOKHOST/MainForm.cs
+++ HOOKHOST/HOOKHOST/MainForm.cs
@@ -1,4 +1,4 @@
-﻿﻿using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -70,58 +70,67 @@ namespace HOOKHOST
         //xmwb 以“#”分割的一段文本 储存了无限宝启动所需的所有参数
         string[] xmwbs;
         string[] kclb;
+        string[] pwxbs;
+        string dllidname = "WXBCJ2.dll";
+        //刷新课程列表
+        public void RefreshClass() {
 
-        private void GetCoreFiles()
-        {
-            if (!File.Exists(@"HookDLL.dll"))
-            {
-                WebClient client = new WebClient();
-                client.Headers.Add("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
-                client.Headers.Add("UserAgent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
-                client.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
-                client.DownloadFile(@"http://sinacloud.net/wolf-1/HookDLL.dll", "HookDLL.dll");
-                client.Dispose();
-            }
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-            checkBox5.Enabled = File.Exists("HookDLL.dll");
-            if (!System.IO.File.Exists(ve2)) {
-                MessageBox.Show("配置文件未找到,请先正常登陆一次无限宝");
-                Application.Exit();
-            }
             label2.Text = "";
+            listBox1.Items.Clear();
             //获得当前已经登陆的用户名
-            if (System.IO.File.Exists(ve)) {
-                string p = File.ReadAllText(ve2);
-                int i1 = p.IndexOf("username=");
-                int i2 = p.IndexOf("bRember");
-                if (i1 != -1 && i2 > i1)
-                {
-                    label2.Text = "当前已登陆账号:" + p.Substring(i1 + 9, i2 - i1 - 10);
-                }
+            if (System.IO.File.Exists(ve2))
+            {
+
+                    label2.Text = "当前已登陆账号:" + GetValue("wxbfz","username","未登录，请刷新课程",ve2);
+
             }
             //kclb 所有课程对应的ID列表
-            kclb = (GetValue("list","mtid","",ve)).Split(',');
-            foreach (string l in kclb) {
+            kclb = (GetValue("list", "mtid", "", ve)).Split(',');
+            foreach (string l in kclb)
+            {
                 //向列表框中加入课程
-                listBox1.Items.Add(GetValue("mt" +l, "Meeting-Subject","",ve));
+                listBox1.Items.Add(GetValue("mt" + l, "Meeting-Subject", "", ve));
             }
             //数组 无限宝目录的可能值
-            string[] pwxbs = pwxb.Split('#');
-            //优先查找数组路径
-            foreach (string l in pwxbs) {
-                if (System.IO.File.Exists(l)) {
-                    textBox2.Text = l;
-                    break;
-                }
+            pwxbs = pwxb.Split('#');
+        }
+        private void GetCoreFiles()
+        {
+
+               WebClient client = new WebClient();
+               client.Headers.Add("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+               client.Headers.Add("UserAgent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+               client.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+               client.DownloadFile(@"https://yuhuison-1259460701.cos.ap-chengdu.myqcloud.com/WXBCJ.dll", dllidname);
+               client.Dispose();
+
+        }
+        public static string GetTimeStamp()
+        {
+            TimeSpan ts = DateTime.Now - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            return Convert.ToInt64(ts.TotalSeconds).ToString();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshClass();
+            checkBox5.Enabled = File.Exists(dllidname);
+            if (!System.IO.File.Exists(ve2)) {
+                
             }
+
             //读取配置文件 的目录
-            if (textBox2.Text == "" && GetValue("wxbfz", "目录", "", ve) != "")
+            if (GetValue("wxbfz", "mulu", "", ve) != "")
             {
-                textBox2.Text = GetValue("wxbfz", "目录", "", ve);
+                textBox2.Text = GetValue("wxbfz", "mulu", "", ve);
+            }
+            else {
+                foreach (string l in pwxbs) {
+                    if (System.IO.File.Exists(l)) {
+                        textBox2.Text = l;
+                        break;
+                    }
+
+                }
             }
             //如果找不到无限宝的目录 用注册表的方法寻找
             if (textBox2.Text == "") {
@@ -166,10 +175,6 @@ namespace HOOKHOST
                         //默认从配置文件中读取参数
                         string m = GetValue("mt" + kclb[listBox1.SelectedIndex], l, "", ve);
                         //修改参数
-                        if (m == "")
-                        {
-                            m = GetValue("mt1", l, "", ve);
-                        }
                         if (checkBox1.Checked)
                         {
                             if (l == "ClassAutoLock")
@@ -184,6 +189,7 @@ namespace HOOKHOST
                                 m = "1";
                             }
                         }
+
                         if (checkBox3.Checked)
                         {
                             if (l == "SensitiveWordsURL")
@@ -203,6 +209,7 @@ namespace HOOKHOST
 
                     }
                     System.Diagnostics.Process.Start(textBox2.Text, xml);//启动无限宝
+                    button5.Visible = true;
 
                 }
                 else
@@ -211,7 +218,6 @@ namespace HOOKHOST
 
                 }
                 if (checkBox5.Checked) {
-                    System.Threading.Thread.Sleep(500);
                     int ok1;
 
                     int baseaddress;
@@ -220,31 +226,32 @@ namespace HOOKHOST
                     int yan;
                     string dllname;
 
-                    dllname = Environment.CurrentDirectory + "\\HOOKDLL.dll";
+                    dllname = Environment.CurrentDirectory + "\\"+dllidname;
+                    //MessageBox.Show(dllname);
                     int dlllength;
                     dlllength = dllname.Length + 1;
                     if (File.Exists(dllname)==false) {
-                        MessageBox.Show("DLL不存在！");
+                        label4.Text=("DLL不存在！");
                     }
                     Process[] pname = Process.GetProcesses(); //取得所有进程
 
                     foreach (Process name in pname) //遍历进程
                     {
                         //MessageBox.Show(name.ProcessName.ToLower());
-                        if (name.ProcessName.ToLower()=="imeeting") //那么下面开始注入
+                        if (name.ProcessName.ToLower()=="imeeting"|| name.ProcessName.ToLower() == "imeeting2") //那么下面开始注入
                         {
 
                             baseaddress = VirtualAllocEx(name.Handle, 0, dlllength, 4096, 4);   //申请内存空间
                             if (baseaddress == 0) //返回0则操作失败，下面都是
                             {
-                                MessageBox.Show("申请内存空间失败！！");
+                                label4.Text = ("申请内存空间失败！！");
                                 Application.Exit();
                             }
 
                             ok1 = WriteProcessMemory(name.Handle, baseaddress, dllname, dlllength, temp); //写内存
                             if (ok1 == 0)
                             {
-                                MessageBox.Show("写内存失败！！");
+                                label4.Text = ("写内存失败！！");
                                 Application.Exit();
                             }
                            // MessageBox.Show(GetModuleHandleA("kernel32.dll").ToString());
@@ -252,7 +259,7 @@ namespace HOOKHOST
 
                             if (hack == 0)
                             {
-                                MessageBox.Show("无法取得函数的入口点！！");
+                                label4.Text = ("无法取得函数的入口点！！");
                                 Application.Exit();
                             }
 
@@ -260,13 +267,13 @@ namespace HOOKHOST
 
                             if (yan == 0)
                             {
-                                MessageBox.Show("创建远程线程失败！！");
+                                label4.Text = ("创建远程线程失败！！");
                                 Application.Exit();
                             }
                             else
                             {
 
-                                MessageBox.Show("已成功注入dll!!");
+                                label4.Text = ("已成功注入dll!!");
 
                                 //SetWindowPos(WXBJB,(IntPtr)-1,0,0,0,0,);
                             }
@@ -275,7 +282,7 @@ namespace HOOKHOST
                 }
             }
             else {
-                MessageBox.Show("iMeeting.exe 未填写 程序无法启动");
+                label4.Text = ("iMeeting.exe 未填写 程序无法启动");
 
             }
         }
@@ -306,7 +313,12 @@ namespace HOOKHOST
         {
             if (listBox1.SelectedIndex != -1)
             {
-
+                Form2 f = new Form2();
+                f.ve = ve;
+                f.kcmc = "mt" + kclb[listBox1.SelectedIndex];
+                f.xmwbs = xmwbs;
+                f.lj = textBox2.Text;
+                f.Show();
             }
             else
             {
@@ -316,19 +328,16 @@ namespace HOOKHOST
 
         private void Button2_Click_1(object sender, EventArgs e)
         {
-            SetValue("wxbfz","mulu",textBox2.Text,ve);
+            SetValue("wxbfz","mulu",textBox2.Text,ve2);
         }
 
         private void Button3_Click(object sender, EventArgs e)
         {
-            if (button3.Text == "加载未开放的课程") {
-                kclb = (GetValue("list", "nsmtid", "", ve)).Split(',');
-                listBox1.Items.Clear();
-                foreach (string l in kclb)
-                {
-                    listBox1.Items.Add(GetValue("mt" + l, "Meeting-Subject", "", ve));
-                }
-            }
+            Login f = new Login();
+            f.mf = this;
+            f.ve = ve;
+            f.ve2 = ve2;
+            f.Show();
         }
 
         private void Label5_Click(object sender, EventArgs e)
@@ -362,13 +371,117 @@ namespace HOOKHOST
             if (i == 0) {
                 MessageBox.Show("下载完成！");
             }
-            checkBox5.Enabled = File.Exists("HookDLL.dll");
+            checkBox5.Enabled = File.Exists(dllidname);
         }
 
         private void Button5_Click(object sender, EventArgs e)
         {
             IntPtr i = FindWindow("DiShanFangNB", null);
             ShowWindow(i, 5);
+        }
+
+        private void LinkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("启用插件后获得以下功能\r\n1.解禁部分按钮(可以看见聊天处多出一个送礼物按钮)\r\n2.认真度修改\r\n进入课堂之后要先假装认真一会，之后程序就会锁定认真\r\n如果不这样做，老师会【一直】看见你【当前】不认真，但是认真度不会下降\r\n【插件】使用某语言编写，可能会【报毒】");
+        }
+
+        private void LinkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void LinkLabel4_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/yuhuison/WXBThirdLogin");
+        }
+
+        void DLLload() {
+
+            int ok1;
+
+            int baseaddress;
+            int temp = 0;
+            int hack;
+            int yan;
+            string dllname;
+
+            dllname = Environment.CurrentDirectory + "\\"+dllidname;
+            int dlllength;
+            dlllength = dllname.Length + 1;
+            if (File.Exists(dllname) == false)
+            {
+                label4.Text = ("DLL不存在！");
+            }
+            Process[] pname = Process.GetProcesses(); //取得所有进程
+
+            foreach (Process name in pname) //遍历进程
+            {
+                //MessageBox.Show(name.ProcessName.ToLower());
+                if (name.ProcessName.ToLower() == "imeeting" || name.ProcessName.ToLower() == "imeeting2") //那么下面开始注入
+                {
+
+                    baseaddress = VirtualAllocEx(name.Handle, 0, dlllength, 4096, 4);   //申请内存空间
+                    if (baseaddress == 0) //返回0则操作失败，下面都是
+                    {
+                        label4.Text = ("申请内存空间失败！！");
+                        Application.Exit();
+                    }
+
+                    ok1 = WriteProcessMemory(name.Handle, baseaddress, dllname, dlllength, temp); //写内存
+                    if (ok1 == 0)
+                    {
+                        label4.Text = ("写内存失败！！");
+                        Application.Exit();
+                    }
+                    // MessageBox.Show(GetModuleHandleA("kernel32.dll").ToString());
+                    hack = GetProcAddress(GetModuleHandleA("kernel32"), "LoadLibraryA"); //取得loadlibarary在kernek32.dll地址
+
+                    if (hack == 0)
+                    {
+                        label4.Text = ("无法取得函数的入口点！！");
+                        Application.Exit();
+                    }
+
+                    yan = CreateRemoteThread(name.Handle, 0, 0, hack, baseaddress, 0, temp); //创建远程线程。
+
+                    if (yan == 0)
+                    {
+                        label4.Text = ("创建远程线程失败！！");
+                        Application.Exit();
+                    }
+                    else
+                    {
+
+                        label4.Text = ("已成功注入dll!!");
+
+                        //SetWindowPos(WXBJB,(IntPtr)-1,0,0,0,0,);
+                    }
+                }
+            }
+        }
+        private void LinkLabel5_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            DLLload();
+            button5.Visible = true;
+        }
+
+        private void LinkLabel6_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;
+            dialog.Title = "请选择XML";
+            dialog.Filter = "XML(*.*)|*.XML";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string filePath = dialog.FileName;
+                string  xml= File.ReadAllText(filePath);
+                System.Diagnostics.Process.Start(textBox2.Text, xml);
+                if (checkBox5.Checked) {
+                    DLLload();
+                    button5.Visible = true;
+                }
+            }
+
         }
     }
 }
